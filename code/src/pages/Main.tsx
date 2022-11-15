f7e55ca980c3ffc565b592c6123c20138db287a5
SalRej <sal3wwm@gmail.com>
2022-11-15 21:32:38 +0200

    added background with falling cards

--- code/src/pages/Main.tsx
+++ code/src/pages/Main.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React , { useEffect , useState}from 'react'
 
-function Main() {
-  return (
-    <div className='background'>
+const Main:React.FC = ()=>{
+
+    const [cardImageUrls,setCardImageUrls] = useState<string[]>([]);
+    const [isLoaded,setIsLoaded] = useState<boolean>(false);
+
+    useEffect(()=>{
+
+        //load n random card images
+        const urls:string[] = [];
+        const types:string[] = ["Spades","Hearts","Clubs","Ace"];
+        for(let i = 0; i <60;i++){
+
+            const randomTypeNumber:number = Math.floor(Math.random()*3);
+            const currentType:string = types[randomTypeNumber];
+
+            const randomCardNumber:number = Math.floor(Math.random()*12) + 1;
+            urls.push(`cards/${currentType}${randomCardNumber}.png`);
+        }
+        setCardImageUrls(urls);
+    },[])
+
+    useEffect(()=>{
+
+        const cardImages = document.querySelectorAll<HTMLElement>('.falling_card');
+        cardImages.forEach(image=>{
+
+            image.style.left=(Math.random()*window.innerWidth).toString() + 'px';
+            image.style.top=(0 - (Math.random()*window.innerHeight)).toString() + 'px';
+            image.style.height=((Math.random()*4)+1).toString() + 'em';
+        })
+
+        setIsLoaded(true);
+    },[cardImageUrls]);
+
+    useEffect(()=>{
         
+    },[isLoaded]);
+
+    return (
+    <div className='background'>
+        {
+            isLoaded &&
+            cardImageUrls.map(url=>{
+                return(<img className="falling_card" src={url}></img>)
+            })
+        }
     </div>
-  )
+    )
 }
 
 export default Main
